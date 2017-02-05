ec9e124d15eda8a8c259c05a62e4f281d6fba7f5
jgontrum <gontrum@me.com>
2017-02-05 16:15:34 +0100

    moving logger setup

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/app.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/app.py
@@ -1,13 +1,36 @@
 import logging
-import logging.handlers
 
+import logstash
 import connexion
+
+from {{cookiecutter.project_slug}} import options
+
 logging.basicConfig(level=logging.INFO)
+
+# Logging configuration
+logger = logging.getLogger('main')
+logger.setLevel(logging.INFO)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '%(asctime)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+logstash_options = options()['logstash']
+ls = logstash.TCPLogstashHandler(
+    host=logstash_options['host'],
+    port=logstash_options['port'])
+ls.setLevel(logging.INFO)
+logger.addHandler(ls)
+
 
 app = connexion.App("{{cookiecutter.project_name}}")
 application = app.app
 app.add_api("config/api.yml",
-            strict_validation=True)
+            strict_validation=True,
+            validate_responses=True)
 
 if __name__ == '__main__':
     app.run(port=8080, server='flask')
